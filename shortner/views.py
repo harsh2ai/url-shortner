a24e9b8bed3997b276c9f9a4cf31d5786c25faf8
harsh2ai <harshris2314@gmail.com>
2021-05-08 23:57:17 +0530

    first deploy

---
+++ shortner/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render, redirect
+import uuid
+
+from django.http import HttpResponse
+from .models import Url
+# Create your views here.
+
+
+def index(request, *args, **kwargs):
+    return render(request, 'index.html')
+
+
+def create(request, *args, **kwargs):
+    if request.method == 'POST':
+        link = request.POST['link']
+        uid = str(uuid.uuid4())[:5]
+        new_url = Url(link=link, uuid=uid)
+        new_url.save()
+        return HttpResponse(uid)
+
+
+def go(request, pk):
+    url_details = Url.objects.get(uuid=pk)
+    return redirect(url_details.link)
